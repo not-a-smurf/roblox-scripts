732f0cab539f8f75577537193aa675e2e15ed078
not-a-smurf <danhernandeznapole@gmail.com>
2026-04-08 07:56:41 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,6 @@
 local games = {
-    [18326509492] = "killstreak", -- replace with actual PlaceId
+    [18326509492] = "killstreak",
+    [130223052405478]   = "fishpond",
 }
 
 local baseUrl = "https://raw.githubusercontent.com/not-a-smurf/roblox-scripts/main/games/"
